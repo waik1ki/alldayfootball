fbc078497695515007bf50010c51c656509689e5
이창익 <>
2022-05-12 22:24:43 +0900

    article api 업데이트

--- src/api/board.js
+++ src/api/board.js
@@ -19,4 +19,45 @@ function fetchSortedBoards(bNum, limit, page, word, sort) {
   });
 }
 
-export { fetchBoards, fetchSortedBoards };
+function createArticle(articleData) {
+  return instance.post('board/write', {
+    thumb: articleData.thumb,
+    title: articleData.title,
+    contents: articleData.contents,
+    pretext: articleData.pretext,
+    author: articleData.author,
+    authorid: articleData.authorid,
+    bNum: articleData.bNum,
+  });
+}
+
+function editArticle(articleData) {
+  return instance.put('board/edit', {
+    id: articleData.id,
+    thumb: articleData.thumb,
+    title: articleData.title,
+    contents: articleData.contents,
+    pretext: articleData.pretext,
+    bNum: articleData.bNum,
+  });
+}
+function deleteArticle(id) {
+  return instance.post('board/delete', {
+    id: id,
+  });
+}
+
+function fetchArticle(num) {
+  return instance.post('board/findone', {
+    seq: num,
+  });
+}
+
+export {
+  fetchBoards,
+  fetchSortedBoards,
+  fetchArticle,
+  createArticle,
+  deleteArticle,
+  editArticle,
+};
