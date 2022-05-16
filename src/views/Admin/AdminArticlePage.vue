c03c1690b8d0e704c04af224a0d66b2c05c97960
이창익 <>
2022-05-16 21:09:09 +0900

    Admin Article 관리 페이지 구조 변경 및 리팩토링

---
+++ src/views/Admin/AdminArticlePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <admin-article-list></admin-article-list>
+</template>
+
+<script>
+import AdminArticleList from '@/components/Admin/Article/AdminArticleList.vue';
+export default {
+  components: { AdminArticleList },
+};
+</script>
+
+<style></style>
