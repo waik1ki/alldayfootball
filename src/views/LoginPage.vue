f384328248ba272943319af1b777c8fe4c0667a5
이창익 <>
2022-05-17 15:46:49 +0900

    로그인 페이지 구조 변경

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import LoginForm from '@/components/Admin/LoginForm.vue';
+import LoginForm from '@/components/LoginForm.vue';
 export default {
   components: {
     LoginForm,
