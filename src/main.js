4a5c5928685ece14f3a637f60e41e58487745c46
이창익 <>
2022-05-25 17:12:07 +0900

    Revert "데이터 포맷 반환 filter 추가"
    This reverts commit cc49ac2a74e497a2da9b8a3f8ae66bb0542244c3.

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import VueCookies from 'vue-cookies';
-import { sliceText, sliceDate, maskingName } from '@/utils/filters';
+import { sliceText, maskingName } from '@/utils/filters';
 
 Vue.filter('sliceText', sliceText);
-Vue.filter('sliceDate', sliceDate);
 Vue.filter('maskingName', maskingName);
 
 Vue.config.productionTip = false;
